88ff4b65ca2f36955ca324bb9642356040a38739
EnderPearlMC <48450374+EnderPearlMC@users.noreply.github.com>
2020-06-24 19:52:17 +0200

    Add files via upload
    Added new sounds effects, polishing, match 4...

---
+++ Match3/Levels/Tiles/TileNum4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Match3.Levels.Tiles
+{
+    class TileNum4 : Tile
+    {
+
+        public TileNum4(int x, int y, int width, int height) : base("num_4", true, 5, x, y, width, height, Assets.LevelTileNum4)
+        {
+            
+        }
+
+    }
+}
